3aee33f514bde1a79e97328dc8e9460c6ee12de2
Stefan Scherer <scherer_stefan@icloud.com>
2019-01-07 07:24:46 +0100

    Update 1.2.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'mkcert'
-$url = 'https://github.com/FiloSottile/mkcert/releases/download/v1.1.2/mkcert-v1.1.2-windows-amd64.exe'
+$url = 'https://github.com/FiloSottile/mkcert/releases/download/v1.2.0/mkcert-v1.2.0-windows-amd64.exe'
 $url64 = $url
-$checksum = '21fa7e25b1a7a6138584107c37a16048cc31e2b1932b78e2d6bf82ccc342bc91'
+$checksum = 'd4e8cdd9327fb0532d1afe465b982ab763b742af9fca31220d4e884069cec269'
 $checksum64 = $checksum
 $checksumType = 'sha256'
 $checksumType64 = $checksumType
